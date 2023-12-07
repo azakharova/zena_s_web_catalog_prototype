b8b85d4a204c53d5258c23997d70fa0758557f06
Anastasiia Zakharova <anastasia.zakharova.01.95@gmail.com>
2023-12-07 10:10:27 +0100

    Update web_catalog.py created a get method to get colors from snowflake added a select picker for colors

--- web_catalog.py
+++ web_catalog.py
@@ -9,4 +9,5 @@ def get_colors():
         my_cur.execute("select distinct color_or_style from zenas_athleisure_db.products.catalog_for_website")
         return my_cur.fetchall()
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 color_selected = streamlit.selectbox("Pick a sweatsuit color or style:", get_colors())
