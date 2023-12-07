e824654953d720af1802493fe23e56bb5cc43f6f
Anastasiia Zakharova <anastasia.zakharova.01.95@gmail.com>
2023-12-07 10:15:32 +0100

    Update web_catalog.py simplified snowflake connection for testing

--- web_catalog.py
+++ web_catalog.py
@@ -4,10 +4,13 @@ import snowflake.connector
 streamlit.title("Zena's Amazing Athleisure Catalog")
 
 # color's list for picker
-def get_colors():
-    with my_cnx.cursor() as cur:
-        my_cur.execute("select distinct color_or_style from zenas_athleisure_db.products.catalog_for_website")
-        return my_cur.fetchall()
+# def get_colors():
+#     with my_cnx.cursor() as cur:
+#         my_cur.execute("select distinct color_or_style from zenas_athleisure_db.products.catalog_for_website")
+#         return my_cur.fetchall()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-color_selected = streamlit.selectbox("Pick a sweatsuit color or style:", get_colors())
+my_cur = my_cnx.cursor()
+my_cur.execute("select distinct color_or_style from catalog_for_website")
+my_colors = my_cur.fetchall()
+color_selected = streamlit.selectbox("Pick a sweatsuit color or style:", my_colors)
