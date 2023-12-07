e593352ab85534de61fdbbda7bcaaa2cce6fba51
Anastasiia Zakharova <anastasia.zakharova.01.95@gmail.com>
2023-12-07 11:38:39 +0100

    Update web_catalog.py added session state to fix selectbox bug

--- web_catalog.py
+++ web_catalog.py
@@ -1,38 +1,45 @@
-import streamlit
+import streamlit as st
 import snowflake.connector
 import pandas
 
-streamlit.title("Zena's Amazing Athleisure Catalog")
+st.title("Zena's Amazing Athleisure Catalog")
 
-# connect to snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# Connect to snowflake
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
-# run a snowflake query and put it all in a var called my_catalog
+# Run a snowflake query and put it all in a var called my_catalog
 my_cur.execute("select distinct color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
 
-# put the dafta into a dataframe
+# Put the data into a dataframe
 df = pandas.DataFrame(my_catalog)
-# streamlit.write(df)
 
-# put the first column into a list
+# Put the first column into a list
 color_list = df[0].values.tolist()
-# print(color_list)
 
-# color picker
-with streamlit.form("color_picker_form", clear_on_submit=True, border=False):
-    option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
-    submit_button = streamlit.form_submit_button(label='Submit')
+# Initialize session state
+if 'selected_option' not in st.session_state:
+    st.session_state.selected_option = None
+
+# Color picker
+with st.form("color_picker_form", clear_on_submit=True, border=False):
+    # Use session_state for the selected option
+    option = st.selectbox('Pick a sweatsuit color or style:', list(color_list), index=color_list.index(st.session_state.selected_option) if st.session_state.selected_option else 0)
+    submit_button = st.form_submit_button(label='Submit')
+
     if submit_button:
-        # image caption
+        # Save the selected option to session_state
+        st.session_state.selected_option = option
+
+        # Image caption
         product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 
-        # return information about selected option
+        # Return information about the selected option
         my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
         product_info = my_cur.fetchone()
 
-        streamlit.image(product_info[0], caption=product_caption, width=400)
-        streamlit.write('Price: $' + str(product_info[1]))
-        streamlit.write('Available sizes: ' + product_info[2])
-        streamlit.write(product_info[3])
+        st.image(product_info[0], caption=product_caption, width=400)
+        st.write('Price: $' + str(product_info[1]))
+        st.write('Available sizes: ' + product_info[2])
+        st.write(product_info[3])
