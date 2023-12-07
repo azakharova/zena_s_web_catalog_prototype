b071b5b2bf516f32eecfab2f2079a909a92e8239
Anastasiia Zakharova <anastasia.zakharova.01.95@gmail.com>
2023-12-07 10:09:32 +0100

    Update web_catalog.py created a get method to get a list of colors added a select picker for colors

--- web_catalog.py
+++ web_catalog.py
@@ -2,3 +2,11 @@ import streamlit
 import snowflake.connector
 
 streamlit.title("Zena's Amazing Athleisure Catalog")
+
+# color's list for picker
+def get_colors():
+    with my_cnx.cursor() as cur:
+        my_cur.execute("select distinct color_or_style from zenas_athleisure_db.products.catalog_for_website")
+        return my_cur.fetchall()
+
+color_selected = streamlit.selectbox("Pick a sweatsuit color or style:", get_colors())
